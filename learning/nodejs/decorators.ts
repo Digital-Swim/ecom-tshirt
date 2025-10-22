794a56e2105a64707af56a4b2bee664d0eeb172e
Ranjit <r.singhnagi@everdreamsoft.com>
2025-10-22 17:49:04 +0200

    re init

---
+++ learning/nodejs/decorators.ts
@@ -0,0 +1,60 @@
+// Class decorator 
+function Controller(prefix: string) {
+    console.log("Controller  called")
+
+    return function (construcor: Function) {
+        console.log("Controller func called")
+
+        console.log(construcor.prototype)
+        construcor.prototype.prefix = prefix
+        console.log(construcor)
+        console.log(`added prefix to constructor ${prefix}`)
+    }
+}
+
+
+function Log(message: string) {
+    console.log("Log called")
+
+    return function (
+        target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    ) {
+        console.log("Log function called")
+
+        console.log(target, propertyKey, descriptor, message)
+        const original: Function = descriptor.value
+        descriptor.value = function (...args: any[]) {
+            console.log(`Log ${message}`)
+            return original.apply(this, args)
+        }
+    }
+}
+
+function Post() {
+    console.log("Post called")
+    return function (...args: any[]) {
+        console.log("Post function called")
+
+        console.log(args)
+    }
+}
+
+@Controller("/user")
+class User {
+    @Log("Getting route called")
+    get(@Post() data1: string, data2:string) {
+        return data1
+    }
+}
+
+const u = new User()
+// @ts-ignore
+//console.log(u.prefix)
+console.log(u.get("test calls"))
+
+// const a = Controller("test")
+// const f = function () { }
+// const b = a(f)
+// console.log(b)
---
+++ services/auth/src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+
+  constructor(private prisma: PrismaService) { }
+
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return this.prisma.users.findUnique({
+      where: {
+        id
+      }
+    })
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
